19d0e36675e61d1b88471f0c5e55db27ec8f5caf
Pavol Mravec <anter1@gmail.com>
2018-01-10 20:30:10 -0800

    initial commit

---
+++ appengine_config.py
@@ -0,0 +1,14 @@
+# Workaround for click library thinking it is on windows when inside google app engine
+
+import os
+import sys
+on_appengine = os.environ.get('SERVER_SOFTWARE','').startswith('Development')
+
+if on_appengine and os.name == 'nt':
+    sys.platform = "Not Windows"
+
+# End workaround
+
+from google.appengine.ext import vendor
+
+vendor.add('lib')
